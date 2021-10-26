bb5c721ba12491c1874acbd43ef81ca51d573f8b
crockeraw <dinosaur@med-admins-Mac-Pro.local>
2021-10-26 13:26:10 -0400

    needed esc char for arg

--- start-env.sh
+++ start-env.sh
@@ -5,7 +5,7 @@ docker build . -t 611-crocker
 docker run --rm \
  -p 8787:8787 \
  -p 8080:8080 \
- -e PASSWORD=${1:-foo}
+ -e PASSWORD=${1:-foo}\
  -v $(pwd):/home/rstudio/proj/\
  -d\
  -t 611-crocker > /tmp/docker-output
