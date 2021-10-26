ab94347c7f4f684b96bb2a1a5af79d82cdfa4670
crockeraw <dinosaur@med-admins-Mac-Pro.local>
2021-10-26 13:22:48 -0400

    default rstudio password now 'foo'; or provide as argument to start-env.sh

--- start-env.sh
+++ start-env.sh
@@ -2,14 +2,12 @@
 
 docker build . -t 611-crocker
 
-export R_PASS=$(strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n'; echo)
-
 docker run --rm \
  -p 8787:8787 \
  -p 8080:8080 \
- -e PASSWORD=$R_PASS
+ -e PASSWORD=${1:-foo}
  -v $(pwd):/home/rstudio/proj/\
  -d\
  -t 611-crocker > /tmp/docker-output
 
-echo login to Rstudio using the address localhost:8787 in a web browser. Your username is Rstudio, your password is $R_PASS.
+echo login to Rstudio using the address localhost:8787 in a web browser. Your username is Rstudio, your password is ${1:-foo}.
