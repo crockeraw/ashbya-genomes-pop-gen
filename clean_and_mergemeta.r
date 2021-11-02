db2ebd570b7bbf4f0fd1f25e2f26c31073e86bb7
crockeraw <dinosaur@med-admins-Mac-Pro.local>
2021-11-02 17:19:09 -0400

    still a mess, just trying to get working

--- clean_and_mergemeta.r
+++ clean_and_mergemeta.r
@@ -5,12 +5,18 @@ vcf <- read.vcfR("proj/source_data/mito.vcf")
 genotypes <- vcf@gt
 # Isolate IDs are stored in VCF with full path from Longleaf
 # Replace with only the REF ID that would be used in the metadata sheet:
-res <- str_match(colnames(genotypes)[-1], "bams/\\s*(.*?)\\s*SGY")[,2]
+res <- str_match(colnames(genotypes)[-1], "bams/\\s*(.*?)\\s*ASGY")[,2]
 colnames(vcf@gt)[-1] <- res
 genotypes <- vcf@gt
 
 meta <- read.table("proj/source_data/metadata.csv", sep = ",", header = TRUE)
 
-all(colnames(vcf@gt)[-1] == meta$REF)
+# Need to remove strains from genotype and meta objects that are unique to either.
+
+a <- tibble(as.data.frame(vcf@gt))
+m <- tibble(meta)
 
 variants_vector <- getFIX(vcf)
+
+# this makes long tibble with entry per individ X locus
+Z <- vcfR2tidy(vcf, format_fields = c("GT", "DP"))
