2fa1ee7e4a6633d41575f4f99d80a411034218fa
crockeraw <dinosaur@med-admins-Mac-Pro.local>
2021-11-16 20:08:11 -0500

    fixed file paths again

--- seq_sum_stats.r
+++ seq_sum_stats.r
@@ -1,32 +1,34 @@
 library("vcfR")
 
-vcf <- read.vcfR("proj/source_data/mito.vcf")
-# convert to genlight object for use with fxns in genlight pkg.
-x <- vcfR2genlight(vcf)
+vcf <- read.vcfR("source_data/mito_all.vcf")
+
 # need to pipe std-out to file. numer non-biallelic variants removed.
 
 # load reference genome and annotations for sick plots. 
 
-dna_file <- 'proj/source_data/mito.fa'
-gff_file <- 'proj/source_data/mito.gff3'
+dna_file <- 'source_data/mito.fa'
+gff_file <- 'source_data/mito.gff3'
 dna <- ape::read.dna(dna_file, format = "fasta")
 gff <- read.table(gff_file, sep="\t", quote="")
 
 # Create a chromR object, produce pre-filtered QC plots
 chrom <- create.chromR(name="Supercontig", vcf=vcf, seq=dna, ann=gff, verbose=TRUE)
-chrom <- proc.chromR(chrom, verbose=TRUE, win.size = 10)
-png("proj/images/pre-filteringQC.png")
+chrom <- proc.chromR(chrom, verbose=TRUE, win.size = 100)
+png("images/pre-filteringQC.png")
 chromoqc(chrom, dp.alpha = 66)
 dev.off()
-png("proj/images/pre-filteringQC_hist.png")
+png("images/pre-filteringQC_hist.png")
 plot(chrom)
 dev.off()
 
 # Quality filter and plot post-filtering QC
-chrom <- masker(chrom, min_QUAL = 100, min_DP = 1000, min_MQ = 50)
-png("proj/images/post-filteringQC.png")
+vcf <- read.vcfR("source_data/mito_vars.vcf")
+chrom <- create.chromR(name="Supercontig", vcf=vcf, seq=dna, ann=gff, verbose=TRUE)
+chrom <- proc.chromR(chrom, verbose=TRUE, win.size = 100)
+png("images/post-filteringQC.png")
 chromoqc(chrom, dp.alpha = 66)
 dev.off()
-png("proj/images/post-filteringQC_hist.png")
+png("images/post-filteringQC_hist.png")
 plot(chrom)
 dev.off()
+
