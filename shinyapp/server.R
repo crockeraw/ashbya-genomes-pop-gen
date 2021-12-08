2757342b4d375b4aed6689dc205ece902ff60070
crockeraw <alex.w.crocker@gmail.com>
2021-12-08 15:59:52 -0500

    new app

---
+++ shinyapp/server.R
@@ -0,0 +1,32 @@
+function(input, output, session) {
+  library(ggplot2)
+  # Combine the selected variables into a new data frame
+  PCA <- c("PC1", "PC2")
+  DAPC <- c("LD1", "LD2")
+  Geographic <- c("LON", "LAT")
+  col <- data.frame(PCA,DAPC,Geographic)
+  
+  d <- as.data.frame(read.csv("../derived_data/shiny_data.csv"))
+  d[is.na(d)] <- FALSE
+  
+  selectedData <- reactive({
+    col[[input$pos]]
+  })
+  
+  colors <- reactive({
+    as.factor(d[[input$color]])
+  })
+  
+  output$plot1 <- renderPlot({
+    my_pal <- RColorBrewer::brewer.pal(n=8, name = "Dark2")
+    x = d[selectedData()[1]]
+    y = d[selectedData()[2]]
+    n = (input$jitter / 100)
+    ggplot(d, aes(x = get(selectedData()[1]), y = get(selectedData()[2]), color = colors(), fill = colors())) + 
+      geom_jitter(size = 4, shape = 21, height=diff(range(x,na.rm=TRUE)*n), width = diff(range(y,na.rm=TRUE))*n) +
+      guides(fill = guide_legend(title = "Cluster"), colour = guide_legend(title = "Cluster")) +
+      theme_bw() + scale_color_manual(values=c(my_pal),na.value="grey") +
+      scale_fill_manual(values=c(paste(my_pal, "66", sep = "")))
+  })
+  
+}
---
+++ shinyapp/ui.R
@@ -0,0 +1,19 @@
+# k-means only works with numerical variables,
+# so don't give the user the option to select
+# a categorical variable
+df <- read.csv("../derived_data/shiny_data.csv")
+
+color <- c("Cluster", "Plant", "Insect")
+position <-c("PCA", "DAPC", "Geographic")
+
+pageWithSidebar(
+  headerPanel('Ashbya genomes exploration'),
+  sidebarPanel(
+    selectInput('color', 'Color by:', color),
+    selectInput('pos', 'Position by:', position, selected = position[[2]]),
+    numericInput('jitter', 'Scatter data for visibility (%)', 5, min = 0, max = 50)
+  ),
+  mainPanel(
+    plotOutput('plot1')
+  )
+)
